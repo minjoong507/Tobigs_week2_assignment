e5f1b93f9d9436a7d6c716a188179ebbbf039f42
albert507 <albert507@naver.com>
2020-07-27 13:33:57 +0900

    start django app

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\alber\desktop\workspace\week2_assignment\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
